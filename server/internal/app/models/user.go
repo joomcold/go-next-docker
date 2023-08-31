52194eb3dd38d6919b2414789fb7390c7a7d89cd
Joompon Sajjaphoch <joom.sajja@gmail.com>
2023-08-31 07:18:06 +0700

    Update a user model

--- server/internal/app/models/user.go
+++ server/internal/app/models/user.go
@@ -5,11 +5,10 @@ import (
 )
 
 type User struct {
-	ID                   uuid.UUID `gorm:"type:uuid;primaryKey"`
-	Name                 string
-	Email                string `gorm:"unique;not null"`
-	Password             string `gorm:"not null"`
-	PasswordConfirmation string `gorm:"not null"`
-	Address              string
+	ID       uuid.UUID `gorm:"type:uuid;primaryKey"`
+	Name     string
+	Email    string `gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+	Address  string
 	Timestamp
 }
