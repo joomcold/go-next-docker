53f55315f23f33fe17173e2f6d38e140684fbdc9
Joompon Sajjaphoch <joom.sajja@gmail.com>
2023-09-01 03:28:21 +0700

    Add logger and update Docker

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/joomcold/go-next-docker/internal/app/router"
 	"github.com/joomcold/go-next-docker/internal/database"
 	"github.com/joomcold/go-next-docker/internal/initializers"
@@ -15,6 +18,12 @@ func init() {
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New())
+	app.Use(requestid.New())
+	app.Use(logger.New(logger.Config{
+		TimeZone: "Asia/Bangkok",
+	}))
 
 	// Assign routes
 	router.SetupRoutes(app)
