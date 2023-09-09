804875c10d5c8d000626942b27791b1bb8ce88db
Joompon Sajjaphoch <joom.sajja@gmail.com>
2023-09-09 09:33:02 +0700

    Add sass and rules

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+
+const nextConfig = {
+  sassOptions: {
+    reactStrictMode: true,
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
 
 module.exports = nextConfig
